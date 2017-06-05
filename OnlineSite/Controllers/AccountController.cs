ebdc65af657afc91bafb406f722b64324f8e401a
Haytham Allos <haytham.allos@gmail.com>
2017-06-04 18:38:36 -0700

    .

--- OnlineSite/Controllers/AccountController.cs
+++ OnlineSite/Controllers/AccountController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Analyzer.Engine.BusinessAccessLayer;
+using Analyzer.Engine.BusinessFacadeLayer;
+using Analyzer.Engine.Common;
+using Analyzer.Engine.DataAccessLayer.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using OnlineSite.Models;
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -7,29 +15,98 @@ namespace OnlineSite.Controllers
 {
     public class AccountController : Controller
     {
+        private IOptions<ConfigSettings> _settings = null;
+
+        public AccountController(IOptions<ConfigSettings> settings)
+        {
+            _settings = settings;
+        }
         public IActionResult Auth()
         {
-           
+
             return View();
         }
 
-        public async Task<IActionResult> Login()
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Register(UserModel userModel)
         {
-            //var claims = new[] {
-            //    new Claim("name", "InsertAccountNameHere"),
-            //    new Claim(ClaimTypes.Role, "InsertRoleHere")
-            //};
+            try
+            {
+                BusFacCore busFacCore = new BusFacCore(_settings);
+                UtilsValidation utilsValidation = new UtilsValidation();
+                BusUser busUser = new BusUser();
+                if ((utilsValidation.IsValidEmail(userModel.Username)) && (busUser.IsValidPasswd(userModel.Password)))
+                {
+                    if (!(userModel.Password.Equals(userModel.ConfirmPassword, StringComparison.Ordinal)))
+                    {
+                        ViewData["PasswordMatch"] = true;
+                    }
+                    else
+                    {
+                        bool UserExist = busFacCore.Exist(userModel.Username);
+                        if (!UserExist)
+                        {
 
-            var claims = new[] {
-                new Claim("name", "InsertAccountNameHere")
-            };
+                            User user = busFacCore.UserCreate(userModel.Username, userModel.Password);
+                            if ((user != null) && (user.UserID > 0))
+                            {
+                                return RedirectToAction("Index");
+                            }
+                            else
+                            {
+                                ViewData["HasError"] = true;
+                            }
+                        }
+                        else
+                        {
+                            ViewData["UserExist"] = true;
+                        }
+                    }
+                }
+                else
+                {
+                    ViewData["InvalidCredentials"] = true;
+                }
 
-            var principal = new ClaimsPrincipal(
-                new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
 
-            await HttpContext.Authentication.SignInAsync("MyCookieMiddlewareInstance", principal);
+            }
+            catch (Exception ex)
+            {
+                ViewData["HasError"] = true;
+            }
+            return RedirectToAction("Index", "Home");
+        }
 
-            return Ok();
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserModel userModel)
+        {
+            BusFacCore busFacCore = new BusFacCore(_settings);
+            BusUser busUser = new BusUser();
+            if ((busUser.IsValidUsername(userModel.Username)) && (busUser.IsValidPasswd(userModel.Password)))
+            {
+                bool UserExist = busFacCore.Exist(userModel.Username);
+                if (UserExist)
+                {
+                    User user = busFacCore.UserAuthenticate(userModel.Username, userModel.Password);
+                    if ((user != null) && (user.UserID > 0))
+                    {
+                        var claims = new[] {
+                            new Claim("name", userModel.Username)
+                        };
+
+                        var principal = new ClaimsPrincipal(
+                            new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+
+                        await HttpContext.Authentication.SignInAsync("MyCookieMiddlewareInstance", principal);
+
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<IActionResult> Logout()
--- OnlineSite/Models/UserModel.cs
+++ OnlineSite/Models/UserModel.cs
@@ -1,9 +1,23 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace OnlineSite.Models
 {
     public class UserModel
     {
+        [Required]
+        [EmailAddress]
         public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        //[Display(Name = "Remember me?")]
+        //public bool RememberMe { get; set; }
     }
 }
